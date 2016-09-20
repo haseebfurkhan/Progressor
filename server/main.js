11d2de97a766d41c736344b72ab6c7a35673e97e
KCI\haseeb mohammed <hmohammed@keycentrix.com>
2016-09-20 12:34:34 -0500

    Bug fixes drag

--- server/main.js
+++ server/main.js
@@ -2,14 +2,17 @@ import { Meteor } from 'meteor/meteor';
 import { Cards } from '../imports/api/cards.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
     //Cards.find().fetch();
     
     Meteor.methods({
         'updateCard': function(taskid,taskstage){
-            Cards.update({'_id':taskid},
-                { $set: { "stage":parseInt(taskstage)}}
-               );
+            if(!isNaN(taskstage))
+            {
+                Cards.update({'_id':taskid},
+               { $set: { "stage":parseInt(taskstage)}}
+              );
+            }
             return false;
         }
     });
